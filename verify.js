f19b1087355248ebab1745b1bc56b2292e8ecd1f
Victor <victor.sorianomendoza@gmail.com>
2017-09-26 22:04:12 -0700

    added verification class

---
+++ verify.js
@@ -0,0 +1,110 @@
+class Verify 
+{
+	constructor()
+	{
+
+	}
+
+	verify_region (region) 
+	{
+		switch (region) 
+		{
+			case 'na':
+			case 'as':
+			case 'eu':
+			case 'oc':
+			case 'sa':
+			case 'sea':
+				return true;
+			case 'all':
+				this.region = REGION.ALL;
+				return true;
+			default:
+				throw new InvalidRegionType(region);
+				return false;
+
+		}
+	}
+
+	verify_seasion (season) 
+	{
+		switch (season) 
+		{
+			case 'current':
+			case '4':
+				this.season = SEASON.EA2017pre4;
+				return true;
+			case 'last':
+			case '3':
+				this.season = SEASON.EA2017pre3;
+				return true;
+			case '2':
+				this.season = SEASON.EA2017pre2;
+				return true;
+			case '1':
+				this.season = SEASON.EA2017pre1;
+				return true;
+			default:
+				throw new InvalidSeasonType(season);
+				return false;
+		}
+	}
+
+	verify_match (match) 
+	{
+		switch (match) 
+		{
+			case 's':
+			case 'solo':
+				this.match = MATCH.SOLO;
+				return true;
+			case 'd':
+			case 'duo':
+				this.match = MATCH.DUO;
+				return true;
+			case 'sq':
+			case 'squad':
+				this.match = MATCH.SQUAD;
+				return true;
+			case 'sf':
+			case 'sfp':
+			case 'solofpp':
+			case 'solo-fpp':
+				this.match = MATCH.SOLOFPP;
+				return true;
+			case 'df':
+			case 'dfp':
+			case 'duofpp':
+			case 'duo-fpp':
+				this.match = MATCH.DUOFPP;
+				return true;
+			case 'sqf':
+			case 'sqfpp': 
+			case 'squadfpp':
+			case 'squad-fpp': 
+				this.match = MATCH.SQUADFPP;
+				return true;
+			default:
+				throw new InvalidMatchType(match);
+				return false;
+
+
+		}
+	}
+
+	verify_all_parameters(stats_parameters)
+	{
+		if (typeof stats_parameters === 'object')
+		{
+			this.verify_region(stats_parameters['region']);
+			this.verify_seasion(stats_parameters['season']);
+			this.verify_match(stats_parameters['match']);
+		}
+		else
+			throw new InvalidStatsParametersType(stats_parameters);
+	}
+}
+
+module.exports = {
+	Verify,
+}
